dbf7aac92fb2bc5e744dbd333ce02970fb86e966
SirJ0han <busqueme002@hotmail.com>
2016-04-06 15:49:15 +0200

    maven compile

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -1,5 +1,6 @@
 package es.uniovi.asw;
 
+
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
