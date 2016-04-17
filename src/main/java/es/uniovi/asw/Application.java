8b4ae3158a970516cf50244ec07d7364face2e3c
cecitudela <cecitudela12@gmail.com>
2016-04-17 14:07:18 +0200

    Página inicio

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -7,29 +7,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import es.uniovi.asw.physicalVotes.dBUpdate.InsertVotesP;
-import es.uniovi.asw.physicalVotes.dBUpdate.WreportR;
-import es.uniovi.asw.physicalVotes.physicalVotesConfig.Insert;
-import es.uniovi.asw.physicalVotes.physicalVotesConfig.InsertPhysicalR;
-import es.uniovi.asw.physicalVotes.physicalVotesConfig.RVotes;
-import es.uniovi.asw.physicalVotes.reportWriter.WreportP;
-
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
-	
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
-  public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Application.class);
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
-    
-    WreportR report1 = new WreportR(new WreportP());
-	Insert r1 = new InsertPhysicalR(new RVotes(), "src/main/java/votacionesFisicas.xlsx");
-	r1.addVoto(new InsertVotesP(report1) );
-  }
-  
-  
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+
+	}
+
 }
--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -7,17 +7,48 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import es.uniovi.asw.physicalVotes.dBUpdate.InsertVotesP;
+import es.uniovi.asw.physicalVotes.dBUpdate.WreportR;
+import es.uniovi.asw.physicalVotes.physicalVotesConfig.Insert;
+import es.uniovi.asw.physicalVotes.physicalVotesConfig.InsertPhysicalR;
+import es.uniovi.asw.physicalVotes.physicalVotesConfig.RVotes;
+import es.uniovi.asw.physicalVotes.reportWriter.WreportP;
 
 @Controller
 public class Main {
 
-  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-  @RequestMapping("/")
-  public ModelAndView landing(Model model) {
-    LOG.info("Landing page access");
-    return new ModelAndView("landing");
-  }
+	@RequestMapping("/")
+	public ModelAndView landing(Model model) {
+		LOG.info("Landing page access");
+		return new ModelAndView("landing");
 
-  
+	}
+
+	@RequestMapping("/user")
+	public ModelAndView Usuarios() {
+		LOG.info("Pagina de usuario");
+		return new ModelAndView("voter");
+
+	}
+
+	@RequestMapping("/admin")
+	public ModelAndView Admin() {
+		LOG.info("Pagina de admin");
+		return new ModelAndView("config");
+
+	}
+
+	@RequestMapping("/physical")
+	public ModelAndView cargarVotosFisicos() throws Exception {
+		
+		WreportR report1 = new WreportR(new WreportP());
+		Insert r1 = new InsertPhysicalR(new RVotes(), "src/test/resources/votacionesFisicas.xlsx");
+		r1.addVoto(new InsertVotesP(report1));
+
+		
+		return new ModelAndView("physical");
+	}
+
 }
